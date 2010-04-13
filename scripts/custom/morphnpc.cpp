0fa3a6b454aefeb66adb1d3b7c9e72a8aa8cb1d9
Apple15 <pongsatorn15@msn.com>
2010-04-13 14:14:25 +0700

    [FIX]13/4/53

--- scripts/custom/morphnpc.cpp
+++ scripts/custom/morphnpc.cpp
@@ -106,6 +106,7 @@ void SendDefaultMenu_npc_morph(Player* pPlayer, Creature* pCreature, uint32 uiAc
 		break;
 	case 1014: //De Morph
 		pPlayer->DeMorph();
+		pPlayer->CLOSE_GOSSIP_MENU();
 		break;
 
 	}
